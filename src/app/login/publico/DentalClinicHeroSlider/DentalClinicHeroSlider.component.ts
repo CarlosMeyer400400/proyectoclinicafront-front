07a0c781b321dd3670c7d6da8b40a71804eff513
arletteCLuna <128724916+arletteCLuna@users.noreply.github.com>
2024-10-01 20:29:36 -0600

    Update_Responsive_Slider

--- src/app/login/publico/DentalClinicHeroSlider/DentalClinicHeroSlider.component.ts
+++ src/app/login/publico/DentalClinicHeroSlider/DentalClinicHeroSlider.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dental-clinic-hero-slider',
   templateUrl: './dentalclinicheroslider.component.html',
   styleUrls: ['./dentalclinicheroslider.component.css']
 })
-export class DentalClinicHeroSliderComponent implements OnInit {
+export class DentalClinicHeroSliderComponent implements OnInit, OnDestroy {
   images = [
     { src: 'assets/images/dentist2.jpg', alt: 'Dentist at work' },
     { src: 'assets/images/consultorio-dentistas.jpeg', alt: 'Modern dental clinic' },
@@ -16,17 +17,24 @@ export class DentalClinicHeroSliderComponent implements OnInit {
 
   currentIndex = 0;
   intervalId: any;
+  isLoggedIn: boolean = false; // Variable para verificar si el usuario ha iniciado sesión
 
-  constructor() {}
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.startAutoSlide();
+    this.checkLoginStatus(); // Verificar si el usuario ha iniciado sesión
+  }
+
+  // Verificar el estado de inicio de sesión
+  checkLoginStatus(): void {
+    this.isLoggedIn = !!localStorage.getItem('token'); // Cambia 'token' por lo que estés usando para gestionar el estado de sesión
   }
 
   startAutoSlide(): void {
     this.intervalId = setInterval(() => {
       this.nextSlide();
-    }, 5000); 
+    }, 5000);
   }
 
   nextSlide(): void {
@@ -39,6 +47,15 @@ export class DentalClinicHeroSliderComponent implements OnInit {
 
   selectSlide(index: number): void {
     this.currentIndex = index;
+  }
+
+  // Función para manejar la redirección al reservar una cita
+  reservarCita(): void {
+    if (this.isLoggedIn) {
+      this.router.navigate(['/user/agendar']); // Redirigir a la página de agendar si ha iniciado sesión
+    } else {
+      this.router.navigate(['/']); // Redirigir a la página principal si no ha iniciado sesión
+    }
   }
 
   ngOnDestroy(): void {
