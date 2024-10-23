2e6fc8aebebc2ab6b0597aced9abb55c17dadf5b
CARLOSMEYER <20210696@uthh.edu.mx>
2024-10-22 19:43:50 -0600

    baus

--- src/app/login/publico/iniciopublico/iniciopublico.component.ts
+++ src/app/login/publico/iniciopublico/iniciopublico.component.ts
@@ -11,8 +11,59 @@ import AOS from 'aos'; // Import 'aos' as default
 })
 export class IniciopublicoComponent implements OnInit {
   servicios: DataServicio[] = [];
+  images = [
+    { src: 'assets/images/dentist2.jpg', alt: 'Dentist at work' },
+    { src: 'assets/images/consultorio-dentistas.jpeg', alt: 'Modern dental clinic' },
+    { src: 'assets/images/young-woman-dentist.jpg', alt: 'Advanced dental technology' },
+    { src: 'assets/images/valoracion.jpg', alt: 'Patient' },
+    { src: 'assets/images/shutterstock_1036178224.jpg', alt: 'Happy patient' }
+  ];
+  
+  currentIndex = 0;
+  intervalId: any;
+  isLoggedIn: boolean = false;
 
   constructor(private router: Router, private loginService: LoginService) { }
+
+
+  // Verificar el estado de inicio de sesión
+  checkLoginStatus(): void {
+    this.isLoggedIn = !!localStorage.getItem('token'); // Cambia 'token' por lo que estés usando para gestionar el estado de sesión
+  }
+
+  startAutoSlide(): void {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  selectSlide(index: number): void {
+    this.currentIndex = index;
+  }
+
+  // Función para manejar la redirección al reservar una cita
+  reservarCita(): void {
+    if (this.isLoggedIn) {
+      this.router.navigate(['/user/agendar']); // Redirigir a la página de agendar si ha iniciado sesión
+    } else {
+      this.router.navigate(['/']); // Redirigir a la página principal si no ha iniciado sesión
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
 
   ngOnInit(): void {
     AOS.init();
@@ -20,6 +71,8 @@ export class IniciopublicoComponent implements OnInit {
       AOS.refresh();
     });
     this.getServicios(); // Llama a getServicios() para obtener los servicios al inicializar el componente
+    this.startAutoSlide();
+    this.checkLoginStatus(); // Verificar si el usuario ha iniciado sesión
   }
 
   getServicios() {
