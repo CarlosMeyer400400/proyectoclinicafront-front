aabe11e9ae468777baa4ab8076faab6f1a5b7351
CARLOSMEYER <20210696@uthh.edu.mx>
2024-10-27 23:37:42 -0600

    pruebastestcafe MEYER 3

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,14 @@
 import { Selector } from 'testcafe';
 
 // Configura el fixture y la página inicial
-fixture `Mi Aplicación`
-  .page `https://clincicadental.netlify.app/#/`; // Cambia esta URL según sea necesario
+fixture `Verificación de URL`
+    .page `https://clincicadental.netlify.app/#/`; // Cambia esta URL según sea necesario
 
+// Define un test
+test('Verifica que la página carga correctamente', async t => {
+    // Obtiene la URL actual
+    const currentUrl = await t.eval(() => window.location.href);
 
-
-
-
-
-
+    // Verifica que la URL actual es la esperada
+    await t.expect(currentUrl).eql('https://clincicadental.netlify.app/#/'); // Cambia la URL si es necesario
+});
