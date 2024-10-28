a53350d588aad0076fa2087e40235d087693e2c8
CARLOSMEYER <20210696@uthh.edu.mx>
2024-10-27 23:13:15 -0600

    pruebastestcafe 2

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,22 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'], // Asegúrate de que esto esté aquí
     plugins: [
       'karma-jasmine',
       'karma-chrome-launcher',
       'karma-jasmine-html-reporter',
-      '@angular-devkit/build-angular',
+      '@angular-devkit/build-angular', // Esto también debe estar aquí
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // Mantiene visible el contexto de Jasmine
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless'], // Cambia a 'Chrome' si es necesario
     singleRun: false,
     restartOnFileChange: true
   });
